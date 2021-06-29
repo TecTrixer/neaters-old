a7cbd3e1e0b1499ec20f3d4b550a0718b02c4a09
TecTrixer <tonihoevedes@gmail.com>
2021-06-29 12:59:12 +0200

    commented && formatted the code

--- src/genotype.rs
+++ src/genotype.rs
@@ -30,13 +30,21 @@ pub struct Genotype {
 }
 
 impl Genotype {
+    // Function for saving a neural network as a JSON file. This can be done before or after the neural network has been trained.
+    // The file can later be loaded back into the program to apply the neural network to a given problem
+    // You can optionally specify a filename or just leave it as default by giving the parameter "None".
     pub fn save(&self, filename_option: Option<String>) -> Result<(), GenotypeError> {
         let filename: String;
         match filename_option {
             Some(file) => filename = file,
             _ => filename = "network.json".to_string(),
         }
-        let file = match OpenOptions::new().write(true).create(true).truncate(true).open(filename) {
+        let file = match OpenOptions::new()
+            .write(true)
+            .create(true)
+            .truncate(true)
+            .open(filename)
+        {
             Ok(filestream) => filestream,
             _ => return Err(GenotypeError::FileSystemError),
         };
@@ -46,6 +54,8 @@ impl Genotype {
         };
     }
 
+    // This function loads a stored neural network so that it can be used or further trained.
+    // It is reading JSON files and you can specify a filename or use the default one by giving the parameter "None".
     pub fn load(filename_option: Option<String>) -> Result<Genotype, GenotypeError> {
         let filename: String;
         match filename_option {
@@ -62,6 +72,10 @@ impl Genotype {
         };
     }
 
+    // Function for creating new blank neural networks with a specific number of input and output nodes.
+    // This function will also create links between all input and output nodes.
+    // You might notice that there is one more input node than specified. This is because there is an additional bias node
+    // which helps the neural network in cases where all inputs are 0 because its value is always 1.
     pub fn new(num_of_input: u32, num_of_output: u32) -> Genotype {
         let mut nodes: Vec<Node> = vec![];
         for i in 0..num_of_input + 1 {
@@ -70,7 +84,7 @@ impl Genotype {
                 node_type: NodeType::Input,
             });
         }
-        for i in num_of_input + 1..num_of_output + num_of_input + 1{
+        for i in num_of_input + 1..num_of_output + num_of_input + 1 {
             nodes.push(Node {
                 number: i,
                 node_type: NodeType::Output,
@@ -79,7 +93,7 @@ impl Genotype {
 
         let mut links: Vec<Link> = vec![];
         for i in 0..num_of_input + 1 {
-            for j in num_of_input + 1..num_of_output + num_of_input + 1{
+            for j in num_of_input + 1..num_of_output + num_of_input + 1 {
                 links.push(Link {
                     disabled: false,
                     innovation_number: 0,
